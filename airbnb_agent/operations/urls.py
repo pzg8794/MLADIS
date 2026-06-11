b5511d3d9115bf69292c87fa9d084d1caac4fd10
Piter Zacari Garcia Bautista <pzg8794@rit.edu>
2026-06-10 21:16:07 -0400

    Add maintenance operations route

--- airbnb_agent/operations/urls.py
+++ airbnb_agent/operations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import WorkboardView
+from .views import MaintenanceWorkItemView, WorkboardView
 
 
 app_name = "operations"
 
 urlpatterns = [
     path("workboard/", WorkboardView.as_view(), name="workboard"),
+    path("maintenance/", MaintenanceWorkItemView.as_view(), name="maintenance"),
 ]
